60302bbae2099118236e0c7b54625b40a0bbb8ca
Anish Kumar <anishelectronics10@gmail.com>
2019-02-05 21:21:35 +0530

    Removed unwanted commented parts of code

--- webTime.py
+++ webTime.py
@@ -1,7 +1,5 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 #https://24timezones.com/time-zone/ist
-#<div class="time"><span>3</span><span>36</span><span>21</span><sup>PM</sup></div><p>Sunday, February 3, 2019</p>
-#//*[@id="cityClock"]
 #<span class="hours">8</span><span class="minutes">52</span><span class="seconds">18</span><sup>am</sup>
 from bs4 import BeautifulSoup 
 import requests
@@ -16,16 +14,4 @@ while True:
     minutes = re.search('<span class=\"minutes\">(.+?)</span>',soup).group(1)
     sec = re.search('<span class=\"seconds\">(.+?)</span>',soup).group(1)
     am_pm = re.search('<sup>(.+?)</sup',soup).group(1)
-
-#    time = soup.get_text()
-#    time = time.split()
     print(("{}:{}:{}{}").format(hours,minutes,sec,am_pm))
-'''
-#print ((soup.find()))
-
-date = re.search('<p>(.+?)</p>',soup)
-if date:
-    date = date.group(1)
-
-print (date)
-'''
